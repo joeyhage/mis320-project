9855bd5ed2dbbfe1e0be8c2fc8871b12fcc2e868
joeyhage <joecool520@gmail.com>
2017-11-21 01:41:58 -0500

    Tenant page design

--- public/js/tenants.js
+++ public/js/tenants.js
@@ -1,6 +1,7 @@
 const tenantSearch = document.getElementById('tenant-search');
 const tenantsTable = document.getElementById('tenants-table');
 const newTenant = document.getElementById('new-tenant');
+const tenantInfo = document.getElementById('tenant-info');
 
 if (tenantSearch) {
 	tenantSearch.addEventListener('submit', event => {
@@ -30,3 +31,52 @@ if (newTenant) {
 		}
 	});
 }
+if (tenantInfo) {
+	document.querySelector('body').addEventListener('click', event => {
+		const {target} = event;
+		if (target.id === 'show-ssn') {
+			const showSsn = document.getElementById('show-ssn');
+			const maskedSsn = document.getElementById('masked-ssn');
+			const fullSsn = document.getElementById('full-ssn');
+
+			if (maskedSsn.style.display === 'none') {
+				maskedSsn.style.display = 'inline';
+				fullSsn.style.display = 'none';
+				showSsn.innerText = 'Show';
+				showSsn.classList.remove('is-success');
+				showSsn.classList.add('is-danger');
+			} else {
+				maskedSsn.style.display = 'none';
+				fullSsn.style.display = 'inline';
+				showSsn.innerText = 'Hide';
+				showSsn.classList.remove('is-danger');
+				showSsn.classList.add('is-success');
+			}
+		} else if (target.classList.contains('expand-collapse')) {
+			expandCollapseTenantInfo(target);
+		} else if (target.nodeName === 'I' || target.nodeName === 'SPAN') {
+			const parent = target.parentNode;
+			const parent2 = parent.parentNode;
+			if (parent.classList.contains('expand-collapse')) {
+				expandCollapseTenantInfo(parent);
+			} else if (parent2.classList.contains('expand-collapse')) {
+				expandCollapseTenantInfo(parent2);
+			}
+		}
+		event.preventDefault();
+	});
+}
+
+const expandCollapseTenantInfo = target => {
+	const content = document.getElementById(target.getAttribute('data-target-id'));
+	setTimeout(() => {
+		target.querySelector('i').classList.toggle('up');
+	});
+	if (target.textContent === 'Collapse') {
+		content.classList.add('is-hidden');
+		target.innerHTML = target.innerHTML.replace('Collapse', 'Expand');
+	} else {
+		content.classList.remove('is-hidden');
+		target.innerHTML = target.innerHTML.replace('Expand', 'Collapse');
+	}
+};
